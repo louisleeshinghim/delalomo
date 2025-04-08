c155a9157820210c175ca03492dd2de6dcf62670
Louis Lee <h1001242@connect.hku.hk>
2025-04-08 18:12:16 +0800

    I2C tester

---
+++ Sketches/i2c_tester.ino
@@ -0,0 +1,56 @@
+#include <Wire.h>
+
+
+
+void setup()
+{
+  pinMode(14, OUTPUT); digitalWrite(14, LOW);                                                     // Turn on i2C
+  pinMode(42, OUTPUT); digitalWrite(42, HIGH);                                                    // Turn on LED
+  Wire.begin();
+  Serial.begin(9600);
+  Serial.println("\nI2C Scanner");
+}
+
+
+
+void loop()
+{
+  byte error, address;
+  int nDevices;
+
+  Serial.println("Scanning...");
+
+  nDevices = 0;
+  for(address = 1; address < 127; address++ )
+  {
+    // The i2c_scanner uses the return value of
+    // the Write.endTransmisstion to see if
+    // a device did acknowledge to the address.
+    Wire.beginTransmission(address);
+    error = Wire.endTransmission();
+
+    if (error == 0)
+    {
+      Serial.print("I2C device found at address 0x");
+      if (address<16)
+        Serial.print("0");
+      Serial.print(address,HEX);
+      Serial.println("  !");
+
+      nDevices++;
+    }
+    else if (error==4)
+    {
+      Serial.print("Unknown error at address 0x");
+      if (address<16)
+        Serial.print("0");
+      Serial.println(address,HEX);
+    }
+  }
+  if (nDevices == 0)
+    Serial.println("No I2C devices found\n");
+  else
+    Serial.println("done\n");
+
+  delay(2000);           // wait 2 seconds for next scan
+}
